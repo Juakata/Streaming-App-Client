1a1a3273dcbbfa36fe886bdc0d754132977ed5d5
Juakata <juakata07@gmail.com>
2020-05-22 13:45:04 -0600

    add StreamCreate

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
 
 const App = () => (
   <div>
     <Switch>
       <Route exact path="/" />
       <Route exact path="/streams/show" component={StreamShow} />
+      <Route exact path="/streams/create" component={StreamCreate} />
     </Switch>
   </div>
 );
