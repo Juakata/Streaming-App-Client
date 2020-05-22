c694b2a3c90ab7d44de371166dd1d368a190bf2f
Juakata <juakata07@gmail.com>
2020-05-22 13:52:28 -0600

    add StreamDelete component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import StreamShow from './streams/StreamShow';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => (
   <div>
@@ -11,6 +12,7 @@ const App = () => (
       <Route exact path="/streams/show" component={StreamShow} />
       <Route exact path="/streams/create" component={StreamCreate} />
       <Route exact path="/streams/edit" component={StreamEdit} />
+      <Route exact path="/streams/delete" component={StreamDelete} />
     </Switch>
   </div>
 );
