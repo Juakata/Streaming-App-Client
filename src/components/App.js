c7d4c3accf0c47ccc95ed2b49dd3cbfa6c22fba2
Juakata <juakata07@gmail.com>
2020-05-22 13:43:26 -0600

    add StreamShow component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import StreamShow from './streams/StreamShow';
 
 const App = () => (
   <div>
     <Switch>
-      <Route path="/" />
+      <Route exact path="/" />
+      <Route exact path="/streams/show" component={StreamShow} />
     </Switch>
   </div>
 );
