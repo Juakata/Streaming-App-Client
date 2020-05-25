78381b732832de99f0c48e66951d999ea154c5c8
Juakata <juakata07@gmail.com>
2020-05-25 16:13:31 -0600

    implement redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root'),
 );
