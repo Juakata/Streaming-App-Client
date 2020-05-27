94ea527052018b955df31021c3800993e577e247
Juakata <juakata07@gmail.com>
2020-05-27 13:00:18 -0600

    add form to create a stream and handle form errors

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => (
-  <div>StreamCreate</div>
-);
+class StreamCreate extends React.Component {
+  renderInput = ({ input, label, meta }) => {
+    const fieldClass = `field ${meta.error && meta.touched ? 'error' : ''}`;
+    return (
+      <div className={fieldClass}>
+        <label htmlFor={input.name}>
+          {label}
+          <input
+            id={input.name}
+            onChange={input.onChange}
+            value={input.value}
+            onBlur={input.onBlur}
+          />
+        </label>
+      </div>
+    );
+  };
 
-export default StreamCreate;
+  onSubmit = formValues => {
+    console.log(formValues);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)} className="ui form error">
+        <Field
+          name="title"
+          component={this.renderInput}
+          label="Title:"
+        />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description:"
+        />
+        <button type="submit" className="ui button primary">
+          Create
+        </button>
+      </form>
+    );
+  }
+}
+
+const validate = ({ title, description }) => {
+  const errors = {};
+  if (!title) {
+    errors.title = 'Title must exist.';
+  }
+  if (!description) {
+    errors.description = 'Description must exist.';
+  }
+  return errors;
+};
+
+StreamCreate.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+};
+
+export default reduxForm({
+  form: 'streamCreate',
+  validate,
+})(StreamCreate);
