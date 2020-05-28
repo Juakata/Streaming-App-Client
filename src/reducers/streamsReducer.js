348839964ecf3088ff864ad3d90681f9d33c8452
Juakata <juakata07@gmail.com>
2020-05-28 13:50:27 -0600

    initialize streams reducer

--- src/reducers/streamsReducer.js
+++ src/reducers/streamsReducer.js
@@ -4,7 +4,7 @@ import {
   EDIT_STREAM, DELETE_STREAM,
 } from '../actions/types';
 
-export default (state, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
