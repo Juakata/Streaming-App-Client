c8fd8fdf5bddba74ce3184195125942009e12f23
Juakata <juakata07@gmail.com>
2020-06-02 14:04:12 -0600

    update show route

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -46,7 +46,9 @@ class StreamList extends React.Component {
         {this.renderAdmin(stream.userId, stream.id)}
         <i className="large middle iligned icon camera" />
         <div className="content">
-          {stream.title}
+          <Link to={`streams/${stream.id}`} className="header">
+            {stream.title}
+          </Link>
           <div className="description">{stream.description}</div>
         </div>
       </div>
