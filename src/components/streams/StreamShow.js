139eec79f96767e963eabb5e8ccb09543121159e
Juakata <juakata07@gmail.com>
2020-06-02 14:12:14 -0600

    show stream

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchStream } from '../../actions';
 
-const StreamShow = () => (
-  <div>StreamShow</div>
-);
+class StreamShow extends React.Component {
+  componentDidMount() {
+    const { fetchStream, match } = this.props;
+    fetchStream(match.params.id);
+  }
 
-export default StreamShow;
+  render() {
+    const { stream } = this.props;
+    if (!stream) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        <h1>{stream.title}</h1>
+        <h5>{stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+StreamShow.propTypes = {
+  fetchStream: PropTypes.func.isRequired,
+  match: PropTypes.instanceOf(Object).isRequired,
+  stream: PropTypes.instanceOf(Object).isRequired,
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id],
+});
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
